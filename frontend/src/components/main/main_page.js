07c80161b73aef767e9d284b67624fb1290e6061
Niko Wells <wellsniko@gmail.com>
2021-07-17 22:11:03 -0700

    react frontend

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Choices Web App</h1>
+                <footer>
+                    Copyright &copy; 2021 Niko Wells
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
